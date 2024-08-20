1d72bd23060368d21cade5807bb6787a4567b933
ayushthe1 <ayushsharmaa101@gmail.com>
2024-08-20 19:52:59 +0530

    working backend

--- contacts-service/main.go
+++ contacts-service/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/ayushthe1/armur/contacts-service/contacts/proto"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 50051: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	proto.RegisterContactsServiceServer(grpcServer, &server{})
+
+	fmt.Println("Contacts gRPC server is running on port 50051...")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve gRPC server: %v", err)
+	}
+}
